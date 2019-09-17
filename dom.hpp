c0f05665cc44c4810880e5b6bcacc9476b5ba920
Samray <samrayleung@gmail.com>
2019-09-18 00:24:31 +0800

    add getElementById() and related test

--- dom.hpp
+++ dom.hpp
@@ -2,138 +2,145 @@
 #ifndef DOUBANCRAWLER_DOM_H
 #define DOUBANCRAWLER_DOM_H
 
+#include <cassert>
 #include <map>
 #include <queue>
 #include <set>
 #include <string>
 #include <utility>
 #include <vector>
-
 #include "strings.hpp"
 namespace doubanCrawler {
 using AttrMap = std::map<std::string, std::string>;
 
 class ElementData {
-   public:
-    ElementData() = default;
-    ElementData(std::string tagName, AttrMap attributes)
-        : tagName(std::move(tagName)), attributes(std::move(attributes)) {}
-    std::string id() {
-        auto result = attributes.find("id");
-        if (result != attributes.end()) {
-            return result->second;
-        } else {
-            return std::string("");
-        }
+ public:
+  ElementData() = default;
+  ElementData(std::string tagName, AttrMap attributes)
+      : tagName(std::move(tagName)), attributes(std::move(attributes)) {}
+  std::string id() {
+    auto result = attributes.find("id");
+    if (result != attributes.end()) {
+      return result->second;
+    } else {
+      return std::string("");
     }
-    std::set<std::string> classes() {
-        auto clazz = attributes.find("class");
-        std::set<std::string> classElement;
-        if (clazz != attributes.end()) {
-            std::string result = clazz->second;
-            doubanCrawler::split(result, classElement, '.');
-        }
-        return classElement;
+  }
+  std::set<std::string> classes() {
+    auto clazz = attributes.find("class");
+    std::set<std::string> classElement;
+    if (clazz != attributes.end()) {
+      std::string result = clazz->second;
+      doubanCrawler::split(result, classElement, '.');
     }
-    [[nodiscard]] const std::string &getTagName() const {
-        return tagName;
-    }
-    [[nodiscard]] const AttrMap &getAttributes() const {
-        return attributes;
-    }
+    return classElement;
+  }
+  [[nodiscard]] const std::string &getTagName() const { return tagName; }
+  [[nodiscard]] const AttrMap &getAttributes() const { return attributes; }
 
-   private:
-    std::string tagName;
-    AttrMap attributes;
+ private:
+  std::string tagName;
+  AttrMap attributes;
 };
 
 typedef struct NodeType {
-   public:
-    bool isText() {
-        return !text.empty();
-    }
-    bool isElementData() {
-        return !isText();
-    }
-    ElementData element;
-    std::string text;
+ public:
+  bool isText() { return !text.empty(); }
+  bool isElementData() { return !isText(); }
+  ElementData element;
+  std::string text;
 } NodeData;
 
 class Node {
-   public:
-    // data common to all nodes;
-    explicit Node(const std::string &str) {
-        nodeData.text = str;
-        ElementData emptyElement;
-        nodeData.element = emptyElement;
-    };
-    explicit Node(const std::string &_name, const AttrMap &_attrMap, std::vector<Node> _children) {
-        children = std::move(_children);
-        nodeData.text = "";
-        ElementData elementData(_name, _attrMap);
-        nodeData.element = elementData;
-    }
+ public:
+  // data common to all nodes;
+  explicit Node(const std::string &str) {
+    nodeData.text = str;
+    ElementData emptyElement;
+    nodeData.element = emptyElement;
+  };
+  explicit Node(const std::string &_name, const AttrMap &_attrMap,
+                std::vector<Node> _children) {
+    children = std::move(_children);
+    nodeData.text = "";
+    ElementData elementData(_name, _attrMap);
+    nodeData.element = elementData;
+  }
 
-    Node(const Node &node) {
-        children = node.children;
-        nodeData = node.nodeData;
-    }
-    [[nodiscard]] std::vector<Node> getChildren() const {
-        return children;
-    }
+  Node(const Node &node) {
+    children = node.children;
+    nodeData = node.nodeData;
+  }
+  virtual ~Node() = default;
+  [[nodiscard]] std::vector<Node> getChildren() const { return children; }
 
-    [[nodiscard]] NodeData getNodeData() const {
-        return nodeData;
-    }
+  [[nodiscard]] NodeData getNodeData() const { return nodeData; }
 
-   private:
-    // data common to all nodes;
-    std::vector<Node> children;
-    // data specific to each node type;
-    NodeData nodeData;
+ private:
+  // data common to all nodes;
+  std::vector<Node> children;
+  // data specific to each node type;
+  NodeData nodeData;
 };
 
 using Elements = std::vector<Node>;
 
-class Document : Node {
-    using Node::Node;
+class Document : public Node {
+  using Node::Node;
 
-   public:
-    // breadth first search
-    Elements getElementsByTag(const std::string &tagName) {
-        Elements elementList;
-        std::queue<Node> queue;
-        Node root = *this;
-        queue.push(root);
-        while (!queue.empty()) {
-            Node tempNode = queue.front();
-            queue.pop();
-            if (tempNode.getNodeData().isElementData() && tempNode.getNodeData().element.getTagName() == tagName) {
-                elementList.emplace_back(tempNode);
-            }
-            if (!tempNode.getChildren().empty()) {
-                for (auto const &childrenNode : tempNode.getChildren()) {
-                    queue.push(childrenNode);
-                }
-            }
+ public:
+  template <class Predicate>
+  Elements getElementsByPredicate(Predicate predicate) {
+    Elements elementList;
+    std::queue<Node> queue;
+    Node root = *this;
+    queue.push(root);
+    while (!queue.empty()) {
+      Node tempNode = queue.front();
+      queue.pop();
+      if (tempNode.getNodeData().isElementData() && predicate(tempNode)) {
+        elementList.emplace_back(tempNode);
+      }
+      if (!tempNode.getChildren().empty()) {
+        for (auto const &childrenNode : tempNode.getChildren()) {
+          queue.push(childrenNode);
         }
-        return elementList;
+      }
     }
+    return elementList;
+  }
 
-   private:
-    static Node *findFirstElementByTagName(const std::string &tagName, Node *node) {
-        if (node->getNodeData().isElementData() && node->getNodeData().element.getTagName() == tagName) {
-            return node;
-        } else {
-            for (auto &childrenNode : node->getChildren()) {
-                Node *found = findFirstElementByTagName(tagName, &childrenNode);
-                if (found != nullptr) {
-                    return found;
-                }
-            }
+  // breadth first search
+  Elements getElementsByTag(const std::string &tagName) {
+    return getElementsByPredicate([&tagName](Node node) -> bool {
+      return node.getNodeData().element.getTagName() == tagName;
+    });
+  }
+
+  Node getElementById(const std::string &id) {
+    Elements elements = getElementsByPredicate([&id](Node node) -> bool {
+      return node.getNodeData().element.id() == id;
+    });
+    assert(elements.size() == 1);
+    return elements[0];
+  }
+
+ private:
+  static Node *findFirstElementByTagName(const std::string &tagName,
+                                         Node *node) {
+    if (node->getNodeData().isElementData() &&
+        node->getNodeData().element.getTagName() == tagName) {
+      return node;
+    } else {
+      for (auto &childrenNode : node->getChildren()) {
+        Node *found = findFirstElementByTagName(tagName, &childrenNode);
+        if (found != nullptr) {
+          return found;
         }
-        return nullptr;
+      }
     }
+    return nullptr;
+  }
 };
 }  // namespace doubanCrawler
 
--- test.cpp
+++ test.cpp
@@ -15,110 +15,131 @@
 #include <string>
 
 void testEqualMacro() {
-    EXPECT_EQ_TRUE(true);
-    EXPECT_EQ_CSTRING("div", "div");
+  EXPECT_EQ_TRUE(true);
+  EXPECT_EQ_CSTRING("div", "div");
 }
 
 void testSplit() {
-    std::string classes = "item.pic.info";
-    std::set<std::string> classSet;
-    doubanCrawler::split(classes, classSet, '.');
-    EXPECT_EQ_UNSIGNED_LONG(3lu, classSet.size());
+  std::string classes = "item.pic.info";
+  std::set<std::string> classSet;
+  doubanCrawler::split(classes, classSet, '.');
+  EXPECT_EQ_UNSIGNED_LONG(3lu, classSet.size());
 }
 
 void testStartsWith() {
-    const std::string source = "tititoto";
-    std::string prefix = "titi";
-    EXPECT_EQ_INT(1, doubanCrawler::startsWith(prefix, source));
-    prefix = "tito";
-    EXPECT_EQ_INT(true, doubanCrawler::startsWith(prefix, source, 2, 6));
+  const std::string source = "tititoto";
+  std::string prefix = "titi";
+  EXPECT_EQ_INT(1, doubanCrawler::startsWith(prefix, source));
+  prefix = "tito";
+  EXPECT_EQ_INT(true, doubanCrawler::startsWith(prefix, source, 2, 6));
 }
 
 void testParseElement() {
-    const std::string source = R"(<div id="main" class="test"></div>)";
-    doubanCrawler::Parser parser(0, source);
-    doubanCrawler::Node node = parser.parseElement();
-    std::string tagName = node.getNodeData().element.getTagName();
-    EXPECT_EQ_CSTRING("div", tagName.c_str());
+  const std::string source = R"(<div id="main" class="test"></div>)";
+  doubanCrawler::Parser parser(0, source);
+  doubanCrawler::Node node = parser.parseElement();
+  std::string tagName = node.getNodeData().element.getTagName();
+  EXPECT_EQ_CSTRING("div", tagName.c_str());
 }
 
 void testParseNode() {
-    const std::string source = R"(<p>hello world</p>)";
-    doubanCrawler::Parser parser(0, source);
-    doubanCrawler::Node node = parser.parseNode();
-    EXPECT_EQ_TRUE(node.getNodeData().isElementData());
-    EXPECT_EQ_TRUE(node.getChildren().size() == 1);
-    doubanCrawler::Node childrenNode = node.getChildren().at(0);
-    EXPECT_EQ_TRUE(childrenNode.getNodeData().isText());
-    EXPECT_EQ_CSTRING("hello world", childrenNode.getNodeData().text.c_str());
+  const std::string source = R"(<p>hello world</p>)";
+  doubanCrawler::Parser parser(0, source);
+  doubanCrawler::Node node = parser.parseNode();
+  EXPECT_EQ_TRUE(node.getNodeData().isElementData());
+  EXPECT_EQ_TRUE(node.getChildren().size() == 1);
+  doubanCrawler::Node childrenNode = node.getChildren().at(0);
+  EXPECT_EQ_TRUE(childrenNode.getNodeData().isText());
+  EXPECT_EQ_CSTRING("hello world", childrenNode.getNodeData().text.c_str());
 }
 
 void testParseAttributes() {
-    const std::string source = R"(<div id="main" class="test"></div>)";
-    doubanCrawler::Parser parser(0, source);
-    std::vector<doubanCrawler::Node> nodes = parser.parseNodes();
-    doubanCrawler::Node node = nodes.at(0);
-    EXPECT_EQ_TRUE(node.getNodeData().isElementData());
+  const std::string source = R"(<div id="main" class="test"></div>)";
+  doubanCrawler::Parser parser(0, source);
+  std::vector<doubanCrawler::Node> nodes = parser.parseNodes();
+  doubanCrawler::Node node = nodes.at(0);
+  EXPECT_EQ_TRUE(node.getNodeData().isElementData());
 }
 
 void testParse() {
-    std::ifstream file("source/parseTest.html");
-    std::stringstream buffer;
-    buffer << file.rdbuf();
-    std::string source = buffer.str();
-    doubanCrawler::Node node = doubanCrawler::parse(source);
+  std::ifstream file("source/parseTest.html");
+  std::stringstream buffer;
+  buffer << file.rdbuf();
+  std::string source = buffer.str();
+  doubanCrawler::Node node = doubanCrawler::parse(source);
 }
 
 void testStringContains() {
-    std::string substring("needle");
-    std::string source("There are two needles in this haystack.");
-    EXPECT_EQ_TRUE(doubanCrawler::contains(substring, source));
+  std::string substring("needle");
+  std::string source("There are two needles in this haystack.");
+  EXPECT_EQ_TRUE(doubanCrawler::contains(substring, source));
 }
 
 void testConsumeComment() {
-    std::ifstream file("source/commentTest.html");
-    std::stringstream buffer;
-    buffer << file.rdbuf();
-    std::string emptyString("");
-    std::regex pattern(R"(<!--.*?-->)");
-    std::string result = std::regex_replace(buffer.str(), pattern, emptyString);
-    std::string commentDescriptor("<!--");
-    EXPECT_EQ_TRUE(!doubanCrawler::contains(commentDescriptor, result));
+  std::ifstream file("source/commentTest.html");
+  std::stringstream buffer;
+  buffer << file.rdbuf();
+  std::string emptyString("");
+  std::regex pattern(R"(<!--.*?-->)");
+  std::string result = std::regex_replace(buffer.str(), pattern, emptyString);
+  std::string commentDescriptor("<!--");
+  EXPECT_EQ_TRUE(!doubanCrawler::contains(commentDescriptor, result));
 }
 
 void testHttp() {
-    /* first where are we going to send it? */
-    const std::string host = "stackoverflow.com";
-    doubanCrawler::http_get(host);
-}
-void testGetElementsByTag() {
-    std::ifstream file("source/parseTest.html");
-    std::stringstream buffer;
-    buffer << file.rdbuf();
-    std::string source = buffer.str();
-    doubanCrawler::Node node = doubanCrawler::parse(source);
-    doubanCrawler::Document *doc = (doubanCrawler::Document *)&node;
-    doubanCrawler::Elements elements = doc->getElementsByTag(std::string("div"));
-    for (auto const &result : elements) {
-        TRACE(("tagName: %s\n", result.getNodeData().element.getTagName().c_str()));
-        doubanCrawler::AttrMap attributes = result.getNodeData().element.getAttributes();
-        for (auto const &keyValuePair : attributes) {
-            TRACE(("key: %s value: %s\n", keyValuePair.first.c_str(), keyValuePair.second.c_str()));
-        }
-    }
+  /* first where are we going to send it? */
+  const std::string host = "stackoverflow.com";
+  doubanCrawler::http_get(host);
 }
 
-int main(int argc, char *argv[]) {
-    testGetElementsByTag();
-    testEqualMacro();
-    testSplit();
-    testStartsWith();
-    testStringContains();
-    testParseElement();
-    testParseNode();
-    testParseAttributes();
-    testParse();
-    testHttp();
-    testConsumeComment();
-    PRINT_TEST_RESULT();
+void printNode(const doubanCrawler::Node &result) {
+  TRACE(("tagName: %s\n", result.getNodeData().element.getTagName().c_str()));
+  doubanCrawler::AttrMap attributes =
+      result.getNodeData().element.getAttributes();
+  for (auto const &keyValuePair : attributes) {
+    TRACE(("key: %s value: %s\n", keyValuePair.first.c_str(),
+           keyValuePair.second.c_str()));
+  }
+}
+
+void testGetElementsByTag() {
+  std::ifstream file("source/parseTest.html");
+  std::stringstream buffer;
+  buffer << file.rdbuf();
+  std::string source = buffer.str();
+  doubanCrawler::Node node = doubanCrawler::parse(source);
+  doubanCrawler::Document *doc = (doubanCrawler::Document *)&node;
+  doubanCrawler::Elements elements = doc->getElementsByTag(std::string("div"));
+  for (auto const &result : elements) {
+    printNode(result);
+  }
+}
+
+void testGetElementById() {
+  std::ifstream file("source/parseTest.html");
+  std::stringstream buffer;
+  buffer << file.rdbuf();
+  std::string source = buffer.str();
+  doubanCrawler::Node node = doubanCrawler::parse(source);
+  doubanCrawler::Document *doc = (doubanCrawler::Document *)&node;
+  doubanCrawler::Node result = doc->getElementById("main");
+  printNode(result);
+  EXPECT_EQ_CSTRING(
+      result.getNodeData().element.getAttributes().at("class").c_str(), "test");
+}
+
+int main(void) {
+  testGetElementsByTag();
+  testGetElementById();
+  testEqualMacro();
+  testSplit();
+  testStartsWith();
+  testStringContains();
+  testParseElement();
+  testParseNode();
+  testParseAttributes();
+  testParse();
+  testHttp();
+  testConsumeComment();
+  PRINT_TEST_RESULT();
 }
